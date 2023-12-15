209b0718f0aa67c4b6655ac37d0036c6a250be7c
Anastasia Pirogova <109249070+Nastia-Pirogova@users.noreply.github.com>
2023-12-16 00:20:34 +0200

    add

--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -1,30 +1,28 @@
 (() => {
-    const mobileMenu = document.querySelector('.js-menu-container');
     const openMenuBtn = document.querySelector('.js-open-menu');
     const closeMenuBtn = document.querySelector('.js-close-menu');
+    const modal = document.querySelector("[data-modal]");
 
     const toggleMenu = () => {
-        const isMenuOpen =
-            openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+        const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
         openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-        mobileMenu.classList.toggle('is-open');
-
-        // const scrollLockMethod = !isMenuOpen
-        //     ? 'disableBodyScroll'
-        //     : 'enableBodyScroll';
-        // bodyScrollLock[scrollLockMethod](document.body);
+        modal.classList.toggle('is-hidden');
     };
 
     openMenuBtn.addEventListener('click', toggleMenu);
     closeMenuBtn.addEventListener('click', toggleMenu);
 
-    // Close the mobile menu on wider screens if the device orientation changes
-    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-        if (!e.matches) return;
-        mobileMenu.classList.remove('is-open');
-        openMenuBtn.setAttribute('aria-expanded', false);
-        bodyScrollLock.enableBodyScroll(document.body);
-    });
-})();
+    const refs = {
+        modal: document.querySelector("[data-modal]"),
+    };
 
+    const toggleModal = () => {
+        refs.modal.classList.toggle("is-hidden");
+    };
+
+    const toggleModalBtn = document.querySelector('.js-toggle-modal');
+    if (toggleModalBtn) {
+        toggleModalBtn.addEventListener('click', toggleModal);
+    }
+})();
 
