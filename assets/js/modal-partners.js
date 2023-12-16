45b7e690331a8ce56ccdd155fb292f09e9d939a3
Anastasia Pirogova <109249070+Nastia-Pirogova@users.noreply.github.com>
2023-12-16 15:03:35 +0200

    fix index.html

---
+++ assets/js/modal-partners.js
@@ -0,0 +1,36 @@
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open-js]"),
+        closeModalBtn: document.querySelector("[data-modal-close-js]"),
+        modal: document.querySelector("[data-modal-js]"),
+        modal1: document.querySelector("#myModal1"),
+        form: document.getElementById("myForm"),
+        closeModalBtn1: document.querySelector("[data-modal-close-js-1]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn1.addEventListener("click", closeThank);
+    refs.form.addEventListener("submit", handleSubmit);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        // toggleModal();
+        refs.modal1.classList.toggle("is-hidden");
+
+
+
+        setTimeout(() => {
+            toggleModal();
+        }, 500);
+    }
+
+    function closeThank() {
+        refs.modal1.classList.toggle("is-hidden");
+    }
+})();
